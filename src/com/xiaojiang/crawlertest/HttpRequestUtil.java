04bdc95519b27ad97b291bd67bf0a649bee2393d
jsf961254858 <961254858@qq.com>
2016-05-11 14:33:22 +0800

    接口url使用httpClient抓取

--- src/com/xiaojiang/crawlertest/HttpRequestUtil.java
+++ src/com/xiaojiang/crawlertest/HttpRequestUtil.java
@@ -1,13 +1,28 @@
 package com.xiaojiang.crawlertest;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.zip.GZIPInputStream;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.http.Header;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.util.EntityUtils;
 import org.json.JSONObject;
+import org.jsoup.nodes.Document;
+
+import com.xiaojiang.exception.DataTaskException;
+import com.xiaojiang.httpclient.HttpClientRequestHandler;
+import com.xiaojiang.httpclient.HttpConnectionManager;
+import com.xiaojiang.httpclient.HttpUserAgent;
 
 
 /**
@@ -69,10 +84,28 @@ public class HttpRequestUtil {
 	}
 	
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		
 		String url = "http://sz.lianjia.com/fangjia/priceTrend/c2411053291137";
-		String result = HttpRequest(url);
+//		String result = HttpRequest(url);
+//		JSONObject json = new JSONObject(result);
+//		Object object = json.getJSONObject("currentLevel")
+//				.getJSONObject("dealPrice")
+//				.get("total");
+//		String s1 = object.toString();
+//		String s2 = s1.substring(1, s1.length()-1).replace("\"", "");
+//		String[] split = s2.split(",");
+//		for(int i = 0;i<split.length;i++){
+//			System.out.println(split[i]);
+//		}
+		HttpClient httpClient = HttpConnectionManager.getHttpClient(null, null);
+		HttpGet httpGet = new HttpGet(url);
+		httpGet.setHeader("Accept-Language", "zh-cn,zh;q=0.5");
+		httpGet.setHeader("Accept-Charset", "GB2312,utf-8;q=0.7,*;q=0.7");
+		httpGet.setHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+		httpGet.setHeader("User-Agent", HttpUserAgent.get());
+		HttpResponse response = httpClient.execute(httpGet);
+		String result  = EntityUtils.toString(response.getEntity(),"GBK");
 		JSONObject json = new JSONObject(result);
 		Object object = json.getJSONObject("currentLevel")
 				.getJSONObject("dealPrice")
@@ -83,8 +116,9 @@ public class HttpRequestUtil {
 		for(int i = 0;i<split.length;i++){
 			System.out.println(split[i]);
 		}
-	
 		
 	}
+	
+	
 	
 }
